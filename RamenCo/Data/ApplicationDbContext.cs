7819f9964912e207de851e620825db0567fc7c4f
rezansoylemez <rezan.sm@gmail.com>
2022-05-01 01:34:42 +0300

    Category Table Added Categories Crud Added

--- RamenCo/Data/ApplicationDbContext.cs
+++ RamenCo/Data/ApplicationDbContext.cs
@@ -14,5 +14,9 @@ namespace RamenCo.Data
         {
         }
         public DbSet<Category> Categories { get; set; }
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Entity<Category>().Property(a => a.Name).HasMaxLength(500).IsRequired();
+        //}
     }
 }
--- RamenCo/Models/BaseEntity.cs
+++ RamenCo/Models/BaseEntity.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace RamenCo.Models
 {
-    public class BaseEntity
+    public abstract class BaseEntity
     {
+        [Key]
         public int ID { get; set; }
     }
 }
